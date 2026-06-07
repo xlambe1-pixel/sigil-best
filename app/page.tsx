c8bd823740679eacac27e013a51c23a6b098cca6
xlambe1-pixel <xlambe1@gmail.com>
2026-06-07 19:35:40 +0300

    move upcoming section above hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from '@/components/Navbar'
 import Hero from '@/components/Hero'
+import UpcomingSection from '@/components/UpcomingSection'
 import CollectionsTable from '@/components/CollectionsTable'
 import Footer from '@/components/Footer'
 
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <main style={{minHeight:'100vh',background:'#080809',color:'#ededf0'}}>
       <Navbar />
+      <UpcomingSection />
       <Hero />
       <CollectionsTable />
       <Footer />
