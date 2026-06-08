d38a55373ba972ffe71f66b85895b1eb828f9218
xlambe1-pixel <xlambe1@gmail.com>
2026-06-08 10:22:12 +0300

    center homepage with maxWidth 1400px

--- app/collection/page.tsx
+++ app/collection/page.tsx
@@ -1,11 +1,17 @@
 import Navbar from '@/components/Navbar'
-import MintPage from '@/components/MintPage'
+import Hero from '@/components/Hero'
+import UpcomingSection from '@/components/UpcomingSection'
+import CollectionsTable from '@/components/CollectionsTable'
+import Footer from '@/components/Footer'
 
-export default function Collection() {
+export default function Home() {
   return (
-    <main style={{minHeight:'100vh',background:'#080809',color:'#ededf0'}}>
+    <main style={{minHeight:'100vh',background:'#080809',color:'#ededf0',maxWidth:'1400px',margin:'0 auto'}}>
       <Navbar />
-      <MintPage />
+      <UpcomingSection />
+      <Hero />
+      <CollectionsTable />
+      <Footer />
     </main>
   )
 }
