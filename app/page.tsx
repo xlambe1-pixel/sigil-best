58415c8751dabc08a94adf5134aa5c995dc4a587
xlambe1-pixel <xlambe1@gmail.com>
2026-06-08 10:57:00 +0300

    add BiggestMovers to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
 import Navbar from '@/components/Navbar'
 import Hero from '@/components/Hero'
 import UpcomingSection from '@/components/UpcomingSection'
+import BiggestMovers from '@/components/BiggestMovers'
 import CollectionsTable from '@/components/CollectionsTable'
 import Footer from '@/components/Footer'
 
 export default function Home() {
   return (
-    <main style={{minHeight:'100vh',background:'#080809',color:'#ededf0'}}>
+    <div style={{background:'#080809',minHeight:'100vh',color:'#ededf0'}}>
       <Navbar />
       <UpcomingSection />
       <Hero />
+      <BiggestMovers />
       <CollectionsTable />
       <Footer />
-    </main>
+    </div>
   )
 }
